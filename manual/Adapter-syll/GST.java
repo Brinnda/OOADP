3a11e9319f48508d4592bf64b77eabecccf6bb1c
someone <someone@someplace.com>
2022-06-28 23:22:51 +0530

    source code

---
+++ manual/Adapter-syll/GST.java
@@ -0,0 +1,9 @@
+
+public class GST implements CalcTax {
+	double tax_amount=0.0;
+	public double taxAmount(String item, int qty, double price) {
+		tax_amount = price*qty*0.18f;
+		return tax_amount;
+	}
+
+}
